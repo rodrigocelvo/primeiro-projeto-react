79e2f010dcfb04f0f2a4ca8b6fa5d320191e4a87
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-07-16 20:19:27 -0300

    Navigate in the routes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParms {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParms>();
+
+  return <h1>Repository: {params.repository}</h1>;
 };
 
 export default Repository;
